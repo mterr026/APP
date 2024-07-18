4e70e09eae1f5f993c7ca210baa7790b7c2c4d8e
mterr026 <mterr026@fu.edu>
2024-07-18 16:19:28 -0400

    version 3

--- src/DB/database.py
+++ src/DB/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost/postgres"
 
 #
 engine = create_engine(
